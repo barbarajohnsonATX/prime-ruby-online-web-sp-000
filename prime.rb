e547db34753b539390219b0968441f24f724cb26
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-06 21:25:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,11 +7,17 @@ def prime?(integer)
       true
     
   
-  i = 2 
-  while i < integer  do 
-    if integer % i == 0 
-      false 
-    end 
-    false 
-  end 
+# Check from 2 to integer-1 
+   i = 2 
+   while i < integer - 1 do 
+     if integer % i == 0 
+       false 
+     end 
+   end 
+   return true 
+    for (int i = 2; i < n; i++) 
+        if (n % i == 0) 
+            return false; 
+  
+    return true; 
 end 
