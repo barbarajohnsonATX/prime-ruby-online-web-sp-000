56f3b61081f36a6189142820385b01d99fbf4515
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-06 21:27:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,22 +2,18 @@
 def prime?(integer)
   # Corner cases 
    if (integer <= 1 || integer == 4)  
-      false
-   if (integer <= 3) 
-      true
+      return false
+  # if (integer <= 3) 
+  #   return  true
     
   
 # Check from 2 to integer-1 
    i = 2 
    while i < integer - 1 do 
      if integer % i == 0 
-       false 
+       return false 
+       i += 1 
      end 
+     return true 
    end 
-   return true 
-    for (int i = 2; i < n; i++) 
-        if (n % i == 0) 
-            return false; 
-  
-    return true; 
 end 
